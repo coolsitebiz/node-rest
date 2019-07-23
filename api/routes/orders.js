4f1529bcc65070f5ea72336b6543407040114784
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2019-07-23 00:22:42 -0700

    started adding routing for super basic rest api

---
+++ api/routes/orders.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'orders were fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'order was created'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(200).json({
+        message: 'order details: ' + id
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(200).json({
+        message: 'order deleted: ' + id
+    });
+});
+
+module.exports = router;
