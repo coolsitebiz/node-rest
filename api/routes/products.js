9f85d8ab874ec463eaed77ac0a5c386d1d34848b
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2019-07-25 17:15:23 -0700

    added db connectivity

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,28 +11,24 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
+    });
+    product.save((err => {
+        if (err) {console.log(err)}
+    }));
     res.status(200).json({
         message: 'Handling POST requests to /products',
         createdProduct: product
     });
 });
 
+//left off at 17:00
 router.get('/:productId', (req, res, next) => {
         const id = req.params.productId;
-        if (id === 'special') {
-            res.status(200).json({
-                message: 'You discovered the special ID',
-                id: id
-            })
-        } else {
-            res.status(200).json({
-                message: 'you passed an ID'
-            })
-        }
+        Product.findById();
 });
 
 router.patch('/:productId', (req, res, next) => {
--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const nodemon = require('./nodemon.json');
+
+mongoose.connect('mongodb+srv://nelsonbm:' + nodemon.env.MONGO_PW + '@cluster0-g2ebr.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
