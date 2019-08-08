3d22670771350f1a4fb950e8f440654760c93f0c
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2019-08-08 15:18:00 -0700

    added authorization token verification

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
+const checkAuth = require('../middleware/check-auth');
 const Order = require('../models/order');
 const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
+
+router.get('/', checkAuth, (req, res, next) => {
     Order.find()
         .select('product quantity _id') 
         .populate('product', 'name')
@@ -32,7 +33,7 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
     Product.findById(req.body.productId)
         .then(product => {
             if(!product) {
@@ -67,7 +68,7 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId)
         .populate('product')
         .exec()
@@ -92,7 +93,7 @@ router.get('/:orderId', (req, res, next) => {
         })
 });
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     const id = req.params.orderId;
     Order.deleteOne({_id: id})
     .exec()
